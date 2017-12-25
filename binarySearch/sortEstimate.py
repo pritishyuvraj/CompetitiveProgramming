679500adb826ba15a7610d47a3e195ad82458d63
Pritish Yuvraj <pritish.yuvi@gmail.com>
2017-12-25 13:04:33 -0500

    commit

--- binarySearch/sortEstimate.py
+++ binarySearch/sortEstimate.py
@@ -1,3 +1,4 @@
 #Question: https://community.topcoder.com/stat?c=problem_statement&pm=3561&rd=6519
 #Question Name: Sort Estimate SortEstimate sortEstimate
 print "Developing solution....."
+print "Testing solution......."
